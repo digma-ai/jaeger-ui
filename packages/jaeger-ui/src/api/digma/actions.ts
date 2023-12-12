f9a280c7d8838df367ee151f28f412ecf479edd9
Kyrylo Shmidt <kshmidt@digma.ai>
2023-12-12 17:45:58 +0100

    Add message to open URL in default browser

--- packages/jaeger-ui/src/api/digma/actions.ts
+++ packages/jaeger-ui/src/api/digma/actions.ts
@@ -1,3 +1,5 @@
+import { addActionPrefix } from './addActionPrefix';
+
 export const actions = {
   GO_TO_SPAN: 'GO_TO_SPAN',
   GO_TO_INSIGHTS: 'GO_TO_INSIGHTS',
@@ -5,3 +7,9 @@ export const actions = {
   SET_SPANS_DATA: 'SET_SPANS_DATA',
   CLEAR: 'CLEAR',
 };
+
+const GLOBAL_ACTION_PREFIX = 'GLOBAL';
+
+export const globalActions = addActionPrefix(GLOBAL_ACTION_PREFIX, {
+  OPEN_URL_IN_DEFAULT_BROWSER: 'OPEN_URL_IN_DEFAULT_BROWSER',
+});
